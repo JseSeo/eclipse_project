445b0bb4c0be1173c165f42791f6c9b0fdf3cfec
wj <6548lha@naver.com>
2024-06-06 19:29:02 +0900

    누락된 MVC 재업로드

---
+++ src/main/java/model/MemberDao.java
@@ -0,0 +1,66 @@
+package model;
+
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import common.JdbcConnectUtil;
+
+public class MemberDao {
+	private static MemberDao mDao;
+	private  Connection con;
+	private PreparedStatement pstmt;
+	private ResultSet rs;
+	private int result;
+	
+  public MemberDao() {
+	  con = JdbcConnectUtil.getConnection();
+	}
+
+public boolean loginCheck(String id, String password) {    	
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+        boolean loginCon = false;
+        try {   	
+            String strQuery = "select id, password from users where id = ? and password = ?";
+            pstmt = con.prepareStatement(strQuery);
+            pstmt.setString(1, id);
+            pstmt.setString(2, password);
+            rs = pstmt.executeQuery();
+            loginCon = rs.next();
+            
+            System.out.println("Login Check: " + loginCon + " for user ID: " + id);
+            
+        } catch (Exception ex) {
+            System.out.println("Exception" + ex);
+        } finally {
+        	JdbcConnectUtil.close(con, pstmt, null);
+        }
+        return loginCon;
+    }	
+	
+    public boolean memberInsert(MemberDTO mDTO) {
+        PreparedStatement pstmt = null;
+        boolean flag = false;
+        try {
+            String strQuery = "insert into users values(?,?,?,?)";
+            pstmt = con.prepareStatement(strQuery);
+            pstmt.setString(1, mDTO.getId());
+            pstmt.setString(2, mDTO.getPassword());
+            pstmt.setString(3, mDTO.getName());
+            pstmt.setString(4, mDTO.getRole());
+            int count = pstmt.executeUpdate();
+            
+            if (count == 1) {
+                flag = true;
+            }
+
+        } catch (Exception ex) {
+            System.out.println("Exception" + ex);
+        } finally {
+        	JdbcConnectUtil.close(con, pstmt, null);
+        }
+        return flag;
+    }		
+	
+}
---
+++ src/main/webapp/registerFinish.jsp
@@ -0,0 +1,30 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+
+<html>
+<head>
+<title>모델 2 예제 (회원가입)</title>
+<style>
+	div#contents {
+		width:300px;
+		height:350px;
+		background-color:#E0F8EC;
+		padding:5px 30px;
+		margin:50px auto;
+		line-height:200%;
+	}
+</style>
+</head>
+<body>
+
+<%@ include file="top.jsp" %>
+<div id="contents"> 
+
+	<h3> 회원가입을 축하합니다!!! </h3>
+	
+</div>
+		
+
+	
+</body>
+</html>
